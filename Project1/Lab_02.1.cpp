3adc455f8329d633784123ad6b114d4c7c5189a1
GTXtab <malahovskinazar007@gmail.com>
2024-09-15 18:04:58 +0300

    Solution for lab

--- Project1/Lab_02.1.cpp
+++ Project1/Lab_02.1.cpp
@@ -19,8 +19,8 @@ int main()
 	cout << "m = "; cin >> m;
 	cout << "n = "; cin >> n;
 
-	// z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(m * m * m * n) + m * n + m * m - m);
-	// z2 = ((sqrt(m) - sqrt(n)) / m);
+	z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(m * m * m * n) + m * n + m * m - m);
+	z2 = ((sqrt(m) - sqrt(n)) / m);
 
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
